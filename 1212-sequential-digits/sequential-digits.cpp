8b984b50f2ccef4fc96c6168d97d5e58b9fafbff
Gunavardhan-Potharlanka <140477906+Gunavardhan-Potharlanka@users.noreply.github.com>
2024-02-02 21:24:23 +0530

    Time: 3 ms (17.15%) | Memory: 7.2 MB (13.81%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
         for(int i=1;i<10;i++){
             int res=0;
-            for(int j=i;j<10;j++){
+            for(int j=i;j<10 && res<high+1;j++){
                 res=res*10+j;
                 if(res>low-1 && res<high+1) ans.push_back(res);}
         }
