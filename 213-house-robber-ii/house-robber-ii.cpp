6d0bb1485891fab29e297f488372b74af1e91223
Gunavardhan-Potharlanka <140477906+Gunavardhan-Potharlanka@users.noreply.github.com>
2024-07-13 15:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.26%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,14 +1,19 @@
 class Solution {
-    int recursion(int ind, int end, vector<int>&nums, vector<int>&dp){
-        if(ind>end-1) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        return dp[ind]=max(nums[ind]+recursion(ind+2, end, nums, dp), recursion(ind+1, end, nums, dp));
+    int solve(vector<int>& nums) {
+        int maxsofar = max(nums[0], nums[1]), max2 = nums[0];
+        for(int i = 2 ; i < nums.size() ; i++) {
+            nums[i] += max2;
+            max2 = maxsofar;
+            maxsofar = max(maxsofar, nums[i]);
+        }
+        return maxsofar;
     }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int> dp1(n+1, -1), dp2(n+1, -1);
-        return max(recursion(0, n-1, nums, dp1), recursion(1, n, nums, dp2));
+        if(nums.size() < 2) return nums[0];
+        if(nums.size() < 3) return max(nums[0], nums[1]);
+        vector<int> dp1 = vector<int>(nums.begin(), nums.end() - 1);
+        vector<int> dp2 = vector<int>(nums.begin() + 1, nums.end());
+        return max(solve(dp1), solve(dp2));
     }
 };
