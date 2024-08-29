077734c8a7b0c43ff80fe23ec8327d5abb37aae6
Gunavardhan-Potharlanka <140477906+Gunavardhan-Potharlanka@users.noreply.github.com>
2024-08-29 16:07:37 +0530

    Time: 7 ms (89.27%) | Memory: 8 MB (65.69%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,49 +1,57 @@
 class Solution {
-    int empty=0;
-    bool column(int r, int c, int key, vector<vector<char>>& board){
-        for(int i=0;i<9;i++) if(board[i][c]-'0'==key) return false;
-        return true;
-    }
-    bool row(int r, int c, int key, vector<vector<char>>& board){
-        for(int i=0;i<9;i++) if(board[r][i]-'0'==key) return false;
-        return true;
-    }
-    bool block(int r, int c, int key, vector<vector<char>>& board){
-        int originx=(r/3)*3, originy=(c/3)*3;
-        for(int i=originx;i<originx+3;i++){
-            for(int j=originy;j<originy+3;j++){
-                if(board[i][j]-'0'==key) return false;
-            }
+    bool empty = true;
+    pair<int, int> nextpos(int r, int c, vector<vector<char>>& board) {
+        c++;
+        if(c > 8) {
+            r++;
+            c = 0;
         }
+        for(int i = r ; i < 9 ; i++)
+            for(int j = (i == r) ? c : 0 ; j < 9 ; j++) if(board[i][j] == '.') return {i, j};
+        return {9, 9};
+    }
+    bool block(int r, int c, int n, vector<vector<char>>& board) {
+        int str = (r / 3) * 3, stc = (c / 3) * 3;
+        for(int i = str ; i - str < 3 ; i++)
+            for(int j = stc ; j - stc < 3 ; j++) if(board[i][j] == '0' + n) return false;
         return true;
     }
-    bool canput(int i, int j, vector<vector<char>>& board, int key){
-        return block(i, j, key, board) && row(i, j, key, board) && column(i, j, key, board);
+    bool col(int c, int n, vector<vector<char>>& board) {
+        for(int i = 0 ; i < 9 ; i++) if(board[i][c] == '0' + n) return false;
+        return true;
     }
-    void search(int row, int col, vector<vector<char>>& board){
+    bool row(int r, int n, vector<vector<char>>& board) {
+        for(int i = 0 ; i < 9 ; i++) if(board[r][i] == '0' + n) return false;
+        return true;
+    }
+    bool clear(int r, int c, int i, vector<vector<char>>& board) {
+        return row(r, i, board) && col(c, i, board) && block(r, c, i, board);
+    }
+    void recursion(int r, int c, vector<vector<char>>& board) {
         if(!empty) return;
-        for(int i=row;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
-                    for(int key=1;key<10;key++){
-                        if(canput(i, j, board, key)){
-                            board[i][j]='0'+key;
-                            empty--;
-                            search(i, j, board);
-                            if(!empty) return;
-                            board[i][j]='.';
-                            empty++;
-                        }
-                    }
-                    if(board[i][j]=='.') return;
+        for(int i = 1 ; i < 10 ; i++) {
+            if(clear(r, c, i, board)) {
+                board[r][c] = '0' + i;
+                pair<int, int> nextinds = nextpos(r, c, board);
+                if(nextinds.first == 9 && nextinds.second == 9) {
+                    empty = false;
+                    return;
                 }
+                recursion(nextinds.first, nextinds.second, board);
+                if(!empty) return;
+                board[r][c] = '.';
             }
         }
-        return;
     }
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++) for(int j=0;j<9;j++) if(board[i][j]=='.') empty++;
-        search(0, 0, board);
+        int i = 0, j = -1;
+        pair<int, int> nextinds = nextpos(i, j, board);
+        if(nextinds.first == 9 && nextinds.second == 9) {
+            empty = false;
+            return;
+        }
+        recursion(nextinds.first, nextinds.second, board);
+        return;
     }
 };
